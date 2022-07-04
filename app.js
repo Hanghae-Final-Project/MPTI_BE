e912cbf473376ebbc04678053d468e1bbcd268a4
pieceofcakey <choi_927@naver.com>
2022-07-05 00:33:03 +0900

    passportConfig() 아래로 내림

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const app = express();
 require('dotenv').config;
 const connect = require('./schemas');
 const kakaoRouter = require('./routes/usersRouter');
-const passport = require('passport')
+const passport = require('passport');
 const passportConfig = require('./passport');
 const session = require('express-session');
 app.use(cors({ origin: true, credentials: true }));
 
 connect();
-passportConfig();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api/comments', require('./routes/commentsRouter.js'));
@@ -18,13 +17,16 @@ app.use('/api/posts', require('./routes/postsRouter.js'));
 app.use('/api', require('./routes/usersRouter'));
 app.use('/api', require('./routes/mypageRouter.js'));
 app.use('/api/kakao', kakaoRouter);
-app.use(session({
-  secret:'mpti',
+app.use(
+  session({
+    secret: 'mpti',
     resave: true,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
+passportConfig();
 
 app.use((req, res, next) => {
   console.log('Request URL:', req.originalUrl, ' - ', new Date());
