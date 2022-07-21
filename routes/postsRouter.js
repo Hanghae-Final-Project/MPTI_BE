8ad8ba020e239f177184b1ac3ea41babab189869
pieceofcakey <choi_927@naver.com>
2022-07-21 18:02:47 +0900

    월, 일 형식 변경

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -84,7 +84,19 @@ router.post(
       }
       const postImage = locationPusher();
 
-      const createdAt = new Date().toLocaleDateString('ko-KR');
+      const now = new Date();
+      const date = now.toLocaleDateString('ko-KR');
+      const year = Number(date.split('.')[0]);
+      let month = Number(date.split('.')[1].trim());
+      if (month < 10) {
+        month = '0' + month;
+      }
+      let day = Number(date.split('.')[2].trim());
+      if (day < 10) {
+        day = '0' + day;
+      }
+      const createdAt = `${year}. ${month}. ${day}`;
+      // const createdAt = new Date().toLocaleDateString('ko-KR');
 
       let countLikes = 0;
 
