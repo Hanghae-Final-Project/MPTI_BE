9097a9ecfa37211fc6974eab7d3dc3b21927fd79
hayan-kim <greenhurb95@naver.com>
2022-07-27 22:30:44 +0900

    session 주석 제거

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(session({
 passportConfig();
 
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   console.log('Request URL:', req.originalUrl, ' - ', new Date());
