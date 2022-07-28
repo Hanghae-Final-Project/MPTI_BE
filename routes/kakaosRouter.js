ee6914dc78d2219820c75bb21e745b0f1aa1706d
hayan-kim <greenhurb95@naver.com>
2022-07-28 23:37:35 +0900

    headers 추가

--- routes/kakaosRouter.js
+++ routes/kakaosRouter.js
@@ -14,8 +14,17 @@ router.get(
   }),
   (req, res) => {
     res.redirect('/');
-    res.header('Access-Control-Allow-Origin', '*')
   }
 );
+exports.handler = async (event) => {
+  const response = {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Origin": 'http://localhost:3000',
+      },
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
 
 module.exports = router;
