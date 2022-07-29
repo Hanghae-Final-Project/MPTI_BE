a90476e69cc9df82cebc5c3a8fa5f3a572d15ae1
hayan-kim <greenhurb95@naver.com>
2022-07-29 15:01:21 +0900

    callback 붙임

--- routes/kakaosRouter.js
+++ routes/kakaosRouter.js
@@ -8,10 +8,30 @@ const KAKAO_REDIRECT_URL = 'https://mptiserver.link/api/kakao/callback'
 
 const router = express.Router();
 
-router.get('/',  (req,res) => {
-  const code = req.params.code
-  res.send(`${ code }`)
-});
+router.get('/callback', function (req, res, next) {
+  let code = req.query.code;
+  console.log(code)
+  try{
+      axios.post(
+          `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${KAKAO_GRANT_TYPE}&client_id=${KAKAO_CLIENT_id}&redirect_uri=${KAKAO_REDIRECT_URL}&code=${code}`
+          , {
+           headers: {
+              'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+          }
+      }).then((result)=>{
+          console.log(result.data['access_token'])
+          // 토큰을 활용한 로직을 적어주면된다.
+
+      }).catch(e=> {
+          console.log(e)
+          res.send(e);
+      })
+  }catch(e){
+      console.log(e)
+      res.send(e);
+  }
+})
+
 
 //소셜 로그인 카카오 구현
 // router.get('/', passport.authenticate('kakao'));
